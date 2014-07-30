ffe5fec5e1e023cdfb4b948f7f06cad9eaaaf93e
Gustav <sir.gustav.the.coder@gmail.com>
2014-07-30 11:07:42 +0200

    spelling fix

--- fastkat/main.cc
+++ fastkat/main.cc
@@ -26,10 +26,10 @@ int main(int argc, char* argv[])
   catch (const Ogre::Exception& e) {
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
     MessageBox(NULL, e.getFullDescription().c_str(),
-               "An exception has occured!",
+               "An exception has occurred!",
                MB_OK | MB_ICONERROR | MB_TASKMODAL);
 #else
-    std::cerr << "An exception has occured: " << e.getFullDescription().c_str()
+    std::cerr << "An exception has occurred: " << e.getFullDescription().c_str()
               << std::endl;
 #endif
   }
